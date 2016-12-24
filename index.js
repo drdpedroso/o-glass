89ebf154c29d78add3d756c7c16bcc24f8b6c1b3
Eduardo Pedroso <drdpedroso@gmail.com>
2016-12-24 19:49:06 -0200

    All working

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
- var program = require('commander');
+ let program = require('commander');
+ let shell = require("shelljs");
 
  program
   .option('-t, --type <type>', 'The application type is')
-
+  .option('-n, --name <name>', 'The application name is')
   .parse(process.argv);
 
-  if(program.type){console.log(program.type)}
+  if(program.type == 'angular'){
+	shell.exec("git clone https://github.com/drdpedroso/ionic-material-skeleton.git");
+	shell.exec("mv ionic-material-skeleton " + program.name);
+	shell.exec("cd " + program.name + " && npm install");
+  }
